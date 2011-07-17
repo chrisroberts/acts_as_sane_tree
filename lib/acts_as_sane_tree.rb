c900d794542817cff65b4555f7515dc0f7398e54
Chris Roberts <chrisroberts.code@gmail.com>
2011-07-17 08:02:49 -0700

    Patch methods into relation if around

--- lib/acts_as_sane_tree.rb
+++ lib/acts_as_sane_tree.rb
@@ -3,4 +3,7 @@ require 'acts_as_sane_tree/version'
 
 if(defined?(Rails))
   ActiveRecord::Base.send :include, ActsAsSaneTree
+  if(defined?(ActiveRecord::Relation))
+    ActiveRecord::Relation.send :include, ActsAsSaneTree
+  end
 end
